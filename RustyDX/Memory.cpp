971d98ab948ccf974294bbb164bf3979640f6888
Allen <28549880+M0rtale@users.noreply.github.com>
2019-08-13 20:09:18 -0400

    Transfer

--- RustyDX/Memory.cpp
+++ RustyDX/Memory.cpp
@@ -30,6 +30,50 @@ static DWORD GetPIDForProcess(const char *Str)
 }
 
 
+bool Memory::Init()
+{
+	LONG64 PID = GetPIDForProcess("RustClient.exe");
+	if (PID == 0)
+	{
+		MessageBoxA(NULL, "ERROR-Process!", NULL, NULL);
+		exit(0);
+	}
+	_Handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, PID);
+	HKEY phkResult;
+	if (ERROR_SUCCESS == RegOpenKeyA(HKEY_LOCAL_MACHINE, "SYSTEM\\ControlSet001\\Services\\DdiMon", &phkResult))
+	{
+		ULONG64 temp_value1 = 0;
+		DWORD lpcbData1 = 8;
+		RegQueryValueExA(phkResult, "PID", NULL, NULL, (LPBYTE)& temp_value1, &lpcbData1);
+		if (temp_value1 != 0)
+		{
+			RegCloseKey(phkResult);
+			printf("Base Address: %llu\n", temp_value1);
+			Base = temp_value1;
+			return true;
+		}
+
+		ULONG64 temp_PID = PID;
+		RegSetValueExA(phkResult, "PID", 0, REG_QWORD, (LPBYTE)& temp_PID, sizeof(ULONG64));
+
+		//force driver to update value
+		char p[100] = { 0 };
+		SIZE_T temp;
+		ReadProcessMemory(_Handle, (void*)0x400000, p, 1, &temp);
+
+		ULONG64 temp_value;
+		DWORD lpcbData = 8;
+
+		RegQueryValueExA(phkResult, "PID", NULL, NULL, (LPBYTE)& temp_value, &lpcbData);
+		Base = temp_value;
+		printf("Base Address: %llu\n", temp_value);
+
+		RegCloseKey(phkResult);
+		return true;
+	}
+	return false;
+}
+
 
 bool Memory::Read_Memory(ULONG64 Addr, void *Buffer, ULONG64 Size)
 {
